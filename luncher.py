b003f40601be35e92c4392b20b26a489e727d67d
Thibaud165 <thibaudj165@gmail.com>
2025-03-01 09:45:09 +0100

    Add files via upload

--- luncher.py
+++ luncher.py
@@ -3,13 +3,13 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/start-server')
+@app.route('/start-server-Survie-Principal-B-J')
 def start_server():
     try:
-        subprocess.Popen(["C:\\chemin\\vers\\ton\\serveur.exe"], shell=True)
-        return "Serveur Minecraft démarré !"
+        subprocess.Popen(["C:\\Users\\thiba\\OneDrive\\Documents\\Serveur\\Minecraft\\Perso\\Survie Principal B-J\\.lunch-server(16Go).bat"], shell=True)
+        return "Serveur Minecraft demmarer (Survie Principal B-J)."
     except Exception as e:
         return f"Erreur : {e}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.1.165', port=5000)
