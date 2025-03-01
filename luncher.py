a6fef5e9e7817820b0fdaa894be1e3360787c9f1
Thibaud165 <thibaudj165@gmail.com>
2025-03-01 10:18:39 +0100

    Add files via upload

--- luncher.py
+++ luncher.py
@@ -1,13 +1,20 @@
-from flask import Flask
 import subprocess
+from flask import Flask
+import os
+
+os.chdir(r"C:\Users\thiba\OneDrive\Documents\Serveur\Minecraft\Perso\Survie Principal B-J")
 
 app = Flask(__name__)
 
 @app.route('/start-server-Survie-Principal-B-J')
 def start_server():
     try:
-        subprocess.Popen(["C:\\Users\\thiba\\OneDrive\\Documents\\Serveur\\Minecraft\\Perso\\Survie Principal B-J\\.lunch-server(16Go).bat"], shell=True)
-        return "Serveur Minecraft demmarer (Survie Principal B-J)."
+        # Chemin absolu vers le fichier .jar de Minecraft
+        subprocess.Popen([
+            "java", "-Xmx16G", "-Xms16G", "-jar", 
+            "C:\\Users\\thiba\\OneDrive\\Documents\\Serveur\\Minecraft\\Perso\\Survie Principal B-J\\paper-1.21.4-136.jar"
+        ], shell=True)
+        return "Serveur Minecraft démarré (Survie Principal B-J)."
     except Exception as e:
         return f"Erreur : {e}"
 
